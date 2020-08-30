3fc643339e4aebfd26cf3207f0784ef2b7d36ca2
gumiranda <imartytk@hotmail.com>
2020-08-30 16:23:35 -0300

    a

--- bin/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ bin/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,32 +1,57 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
-import { sign } from 'crypto';
 jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve('any_token'));
+  },
+  async verify(): Promise<string> {
+    return new Promise((resolve) => resolve('any_value'));
   },
 }));
 const makeSut = (): JwtAdapter => {
   return new JwtAdapter('secret');
 };
-describe('Name of the group', () => {
-  test('should call sign with correct values', async () => {
-    const sut = makeSut();
-    const signSpy = jest.spyOn(jwt, 'sign');
-    await sut.generate('any_id');
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
-  });
-  test('should return a token on sign success', async () => {
-    const sut = makeSut();
-    const accessToken = await sut.generate('any_id');
-    expect(accessToken).toBe('any_token');
-  });
-  test('should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret');
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error();
+describe('jwt geral', () => {
+  describe('sign()', () => {
+    test('should call sign with correct values', async () => {
+      const sut = makeSut();
+      const signSpy = jest.spyOn(jwt, 'sign');
+      await sut.generate('any_id');
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
     });
-    const promise = sut.generate('any_id');
-    await expect(promise).rejects.toThrow();
+    test('should return a token on sign success', async () => {
+      const sut = makeSut();
+      const accessToken = await sut.generate('any_id');
+      expect(accessToken).toBe('any_token');
+    });
+    test('should throw if sign throws', async () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const promise = sut.generate('any_id');
+      await expect(promise).rejects.toThrow();
+    });
+  });
+  describe('verify()', () => {
+    test('should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      await sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
+    });
+    test('should return a value on verify success', async () => {
+      const sut = makeSut();
+      const value = await sut.decrypt('any_token');
+      expect(value).toBe('any_value');
+    });
+    test('should throw if verify throws', async () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const promise = sut.decrypt('any_token');
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
--- bin/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ bin/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,9 +1,14 @@
 import { TokenGenerator } from '../../../protocols/crypto/token-generator';
 import jwt from 'jsonwebtoken';
-export class JwtAdapter implements TokenGenerator {
+import { TokenDecrypter } from '../../../protocols/crypto/token-decrypter';
+export class JwtAdapter implements TokenGenerator, TokenDecrypter {
   private readonly secret: string;
   constructor(secret: string) {
     this.secret = secret;
+  }
+  async decrypt(token: string): Promise<string> {
+    const value: any = await jwt.verify(token, this.secret);
+    return value;
   }
   async generate(id: string): Promise<string> {
     const accessToken = await jwt.sign(
